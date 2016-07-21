0e0f0c84291cd148df781abe42a95a363d3b0295
CooLNuanfeng <1634898698@qq.com>
2016-07-21 17:22:25 +0800

    pro img upload

--- core/pro.inc.php
+++ core/pro.inc.php
@@ -41,6 +41,8 @@
                         addAlbum($albumInfo);
                     }
                 }
+                $mes = '<p>添加成功</p><a href="addPro.php">继续添加</a>|<a href="listPro.php">查看商品列表</a>';
+                return $mes;
             }else{ //产品信息没有成功存入数据库，要将上传的文件删除掉
                 foreach ($fileArr as $key => $value) {
                     if($value['status']){
@@ -52,6 +54,8 @@
                         }
                     }
                 }
+                $mes = '<p>添加失败</p><a href="addPro.php">重新添加</a>|<a href="listPro.php">查看商品列表</a>';
+                return $mes;
             }
 
 
